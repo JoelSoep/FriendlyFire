9e991ed2571696f88f8138416a30f3e7d575d4cc
JoelSoep <92045716+JoelSoep@users.noreply.github.com>
2023-12-12 22:33:56 +0100

    clear players tags when shutting down server

--- src/main/java/me/joelsoep/friendlyfire/Friendlyfire.java
+++ src/main/java/me/joelsoep/friendlyfire/Friendlyfire.java
@@ -5,6 +5,7 @@ import me.joelsoep.friendlyfire.command.FFCommand;
 import me.joelsoep.friendlyfire.listener.EntityDamageByEntityListener;
 import me.joelsoep.friendlyfire.listener.PlayerJoinListener;
 import me.joelsoep.friendlyfire.listener.PlayerQuitListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
@@ -24,5 +25,12 @@ public final class Friendlyfire extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
 
+    }
+    
+    @Override
+    public void onDisable() {
+        playerList.forEach((name, player) -> {
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tab player " + name + " abovename  ");
+        });
     }
 }
